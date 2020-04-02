e5a1cfdfd744531f5c49c38e717a12c93a2dc0fa
Michal Kalita <kalita.michal@gmail.com>
2020-04-02 10:35:33 +0200

    Run simulator server and browser together

--- simulator/rollup.config.js
+++ simulator/rollup.config.js
@@ -1,17 +1,17 @@
 // rollup.config.js
-// import commonjs from "@rollup/plugin-commonjs";
-// import resolve from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
+import resolve from "@rollup/plugin-node-resolve";
 import typescript from "@rollup/plugin-typescript";
 
 export default {
   input: "src/browser.ts",
   output: {
-    dir: "build",
+    dir: "dist",
     format: "iife",
   },
   plugins: [
-    // resolve(),
-    // commonjs(),
     typescript(),
+    commonjs({include: "./node_modules"}),
+    resolve(),
   ],
 };
