ed6d071afbe59b9b43027d2b5c813860bbd19469
Michal Kalita <kalita.michal@gmail.com>
2020-04-02 12:08:00 +0200

    Simulator server log messages

--- simulator/src/server.ts
+++ simulator/src/server.ts
@@ -7,20 +7,32 @@ let publicWs: WebSocket | null = null;
 let privateWs: WebSocket | null = null;
 
 privateWss.on("connection", ws => {
+  console.log("Opened private connection");
   if (!privateWs) privateWs = ws;
 
   ws.on("message", (_, data) => {
-    if (publicWs) publicWs.send(data);
-    else ws.send({ connection_status: "INCOMPLETE" });
+    if (publicWs) {
+      console.log("Forward private -> public", data);
+      publicWs.send(data);
+    } else {
+      console.log("Cannot forward private -> public", data);
+      ws.send({ connection_status: "INCOMPLETE" });
+    }
   });
 });
 
 publicWss.on("connection", ws => {
+  console.log("Opened public connection");
   if (!publicWs) publicWs = ws;
 
   ws.on("message", (_, data) => {
-    if (privateWs) privateWs.send(data);
-    else ws.send({ connection_status: "INCOMPLETE" });
+    if (privateWs) {
+      console.log("Forward public -> private", data);
+      privateWs.send(data);
+    } else {
+      console.log("Cannot forward public -> private", data);
+      ws.send({ connection_status: "INCOMPLETE" });
+    }
   });
 });
 
