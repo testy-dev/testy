28f391f969c69f1b672597ef324ffc4f08410e28
Michal Kalita <kalita.michal@gmail.com>
2020-05-21 20:30:19 +0200

    ext - save graph to graphql

--- chrome-extension/src/popup/components/styled-components.tsx
+++ chrome-extension/src/popup/components/styled-components.tsx
@@ -1,14 +1,14 @@
 import styled from "styled-components";
 
 export const Button = styled.button`
-  border: 1px solid black;
-  background: white;
+  border: ${({ disabled }) => `1px solid ${disabled ? "gray" : "black"}`};
+  background: ${({ disabled }) => (disabled ? "#e7e7e7" : "#ffffff")};
   padding: 5px 7px;
-  cursor: pointer;
+  cursor: ${({ disabled }) => (disabled ? "initial" : "pointer")};
   transition: 300ms background-color;
 
   &:hover {
-    background: #c2c2c2;
+    background: ${({ disabled }) => (disabled ? "#e7e7e7" : "#c2c2c2")};
   }
 `;
 
