9822fb2461759ae26d4c041c8154edc9cc6fef46
Jan Turon <jan.turon@profiq.com>
2020-05-27 15:02:01 +0200

    Add cypress reporter to upload at hasura

---
+++ runner/cypress/plugins/testy_reporter.js
@@ -0,0 +1,54 @@
+const fetch = require("node-fetch");
+const GRAPHQL_ENDPOINT = process.env.GRAPHQL_ENDPOINT || "";
+const HASURA_ADMIN_SECRET = process.env.HASURA_ADMIN_SECRET || "";
+
+exports = module.exports = Base;
+
+function Base(runner) {
+  if (!runner) {
+    throw new TypeError("Missing runner argument");
+  }
+  const report = [];
+  let suiteId = "";
+
+  runner.on("suite", function (suite) {
+    suiteId = suite.title;
+  });
+
+  runner.on("pass", function (test) {
+    console.log(test);
+    report.push({ id: test.title, status: "PASS" });
+  });
+
+  runner.on("fail", function (test, err) {
+    console.log(test);
+    console.log(err);
+    report.push({ id: test.title, status: "FAIL", msg: err.message });
+  });
+
+  runner.on("end", function () {
+    const query = `
+        mutation MyMutation($edges: jsonb, $id: bigint) {
+          update_run_path(where: {id: {_eq: $id}}, _set: {edges: $edges}) {
+            returning {
+              id
+            }
+          }
+        }
+      `;
+    const variables = { id: suiteId, edges: report };
+    console.log(suiteId);
+    console.log(report);
+
+    fetch(GRAPHQL_ENDPOINT, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "x-hasura-admin-secret": HASURA_ADMIN_SECRET,
+      },
+      body: JSON.stringify({ query, variables }),
+    }).then(resp => {
+      console.log(resp);
+    });
+  });
+}
