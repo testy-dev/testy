38b8370983d442a86b22226d0cf68f08a9c081f0
Michal Kalita <kalita.michal@gmail.com>
2020-05-27 10:03:39 +0200

    types in own component

---
+++ shared/src/constants.ts
@@ -0,0 +1,19 @@
+export enum EventType {
+  CLICK = "click",
+  CHANGE = "change",
+  DBCLICK = "dbclick",
+  KEYDOWN = "keydown",
+  SUBMIT = "submit",
+  MOUSEUP = "mouseup",
+}
+
+export enum ControlAction {
+  START = "startRec",
+  STOP = "stopRec",
+  RESET = "resetRec",
+  DELETE = "deleteBlock",
+  MOVE = "moveBlock",
+  PUSH = "pushBlock",
+  HIGHLIGHT = "highlight",
+  REPLAY = "replay",
+}
---
+++ shared/src/types.ts
@@ -0,0 +1,52 @@
+import { ControlAction } from "./constants";
+
+export type RecState = "off" | "on" | "paused";
+
+export interface ParsedEvent {
+  selector: string;
+  action: string;
+  tag: string;
+  value: string;
+  id?: string;
+  key?: string;
+  href?: string;
+  inputType?: string;
+  selectedText?: string;
+}
+
+export interface Session {
+  isPending: boolean;
+  lastURL: string;
+  originalHost?: string;
+}
+
+export interface ActionWithPayload {
+  type: ControlAction;
+  payload?: any;
+}
+
+export const Commands = {
+  visit: "Visit",
+  click: "Click",
+  dblclick: "Double click",
+  type: "Type",
+  submit: "Submit",
+  "check-contains-text": "Contains text",
+};
+
+export interface Block {
+  id: string;
+  command:
+    | "visit"
+    | "click"
+    | "dblclick"
+    | "type"
+    | "submit"
+    | "check-contains-text";
+  selector?: string;
+  parameter?: string;
+}
+
+export type UUID = string;
+export type Color = string;
+export type Edge = [UUID, UUID] | [UUID, UUID, Color];
