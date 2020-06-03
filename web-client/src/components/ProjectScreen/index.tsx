8643aaa2030d72a9b65fd4ee4e41288dedfd94dc
Michal Kalita <kalita.michal@gmail.com>
2020-06-03 21:01:56 +0200

    web-client state of blocks in diagram

--- web-client/src/components/ProjectScreen/index.tsx
+++ web-client/src/components/ProjectScreen/index.tsx
@@ -159,6 +159,53 @@ const ProjectHistory: React.FC<ProjectHistoryProps> = ({
       variables: { orgSlug, projectSlug },
     }
   );
+  const handleOpen = (run: any) => {
+    const counter = run.paths.reduce(
+      (
+        acc: {
+          success: { [blockID: string]: number };
+          fail: { [blockID: string]: number };
+        },
+        path: any
+      ) => {
+        const edges = JSON.parse(path.edges);
+        edges.forEach((block: any) => {
+          if (block.state === "success") {
+            acc.success[block.id] = acc.success[block.id] + 1 || 1;
+          }
+          if (block.state === "failed") {
+            acc.fail[block.id] = acc.fail[block.id] + 1 || 1;
+          }
+        });
+        return acc;
+      },
+      { success: {}, fail: {} }
+    );
+
+    const graph = {
+      edges: run.graph.edges,
+      blocks: run.graph.blocks.map((block: any) => {
+        const success = counter.success?.[block.id] ?? 0;
+        const fail = counter.fail?.[block.id] ?? 0;
+        // success 0, fail 0 => unknown
+        // success > 0, fail 0 => success
+        // success 0, fail > 0 => fail
+        // success > 0, fail > 0 => warning
+        return {
+          ...block,
+          status:
+            success > 0
+              ? fail > 0
+                ? "warning"
+                : "success"
+              : fail > 0
+              ? "fail"
+              : "unknown",
+        };
+      }),
+    };
+    onOpenHistory(run.id, graph);
+  };
 
   if (loading) return <Text>Loading...</Text>;
 
@@ -178,7 +225,7 @@ const ProjectHistory: React.FC<ProjectHistoryProps> = ({
             key={run.id}
             background="light-1"
             pad="small"
-            onClick={() => onOpenHistory(run.id, run.graph)}
+            onClick={() => handleOpen(run)}
             border={{
               side: "left",
               size: "medium",
