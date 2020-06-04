ece360436da9ff3e272c1439a60b3f418c458eda
Jan Turon <jan.turon@profiq.com>
2020-06-04 14:53:35 +0200

    Add sign out to client

--- web-client/src/components/Logo.tsx
+++ web-client/src/components/Logo.tsx
@@ -1,10 +1,16 @@
 import React from "react";
 
-import { Heading } from "grommet";
+import { Button, Heading } from "grommet";
+import firebase from "firebase/app";
 
 const Logo: React.FC = () => (
   <Heading color="#0B74C2" level={2} margin="none">
     Testy
+    <Button
+      primary
+      label="Log Out"
+      onClick={async () => firebase.auth().signOut()}
+    />
   </Heading>
 );
 
