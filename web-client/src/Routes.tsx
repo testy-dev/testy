e312636c4d761f21937a851da51e9388a97768be
Jan Turon <jan.turon@profiq.com>
2020-06-16 14:00:36 +0200

    Add registration screen

--- web-client/src/Routes.tsx
+++ web-client/src/Routes.tsx
@@ -8,6 +8,7 @@ import HomeScreen from "./components/HomeScreen";
 import LoginScreen from "./components/LoginScreen";
 import NotFoundScreen from "./components/NotFoundScreen";
 import ProjectScreen from "./components/ProjectScreen";
+import RegistrationScreen from "./components/RegistrationScreen";
 
 import { fetchQuery } from "./graphql";
 import UserID from "./components/UserID";
@@ -82,7 +83,9 @@ const Routes: React.FC = () => {
         setAuthState("in");
         if (history.location.pathname === "/login") history.push("/");
       } else {
-        if (history.location.pathname !== "/login") history.push("/login");
+        if (!["/login", "/register"].includes(history.location.pathname)) {
+          history.push("/login");
+        }
         setAuthState("out");
       }
     })
@@ -102,6 +105,7 @@ const Routes: React.FC = () => {
     return (
       <Switch>
         <Route path="/login" component={LoginScreen} />
+        <Route path="/register" component={RegistrationScreen} />
         <Route component={NotFoundScreen} />
       </Switch>
     );
