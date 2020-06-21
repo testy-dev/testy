abef12a11aaf0784c89d664ff0eec651a541d4c6
Michal Kalita <kalita.michal@gmail.com>
2020-06-21 13:36:19 +0200

    web - urql as graphql request library

--- web-client/src/App.tsx
+++ web-client/src/App.tsx
@@ -1,18 +1,9 @@
 import React from "react";
 
-import {
-  ApolloClient,
-  ApolloProvider,
-  HttpLink,
-  InMemoryCache,
-  split,
-} from "@apollo/client";
 import { BrowserRouter } from "react-router-dom";
 import { Grommet, generate, grommet } from "grommet";
-import { WebSocketLink } from "@apollo/link-ws";
+import { Provider as UrqlProvider } from "urql";
 import { deepMerge } from "grommet/utils";
-import { getMainDefinition } from "@apollo/client/utilities";
-import { setContext } from "@apollo/link-context";
 
 import "firebase/auth";
 import firebase from "firebase/app";
@@ -20,58 +11,9 @@ import firebase from "firebase/app";
 import { CommandsProvider } from "./components/state/commands";
 import { firebaseConfig } from "./config";
 import Routes from "./Routes";
+import client from "./urqlClient";
 
 firebase.initializeApp(firebaseConfig);
-
-const httpLink = new HttpLink({
-  uri: process.env.REACT_APP_GRAPHQL_ENDPOINT,
-});
-
-const wsLink = new WebSocketLink({
-  uri: process.env.REACT_APP_GRAPHQL_ENDPOINT?.replace("http", "ws") ?? "",
-  options: {
-    reconnect: true,
-    lazy: true,
-    connectionParams: async () => ({
-      headers: {
-        authorization:
-          "Bearer " + (await firebase.auth().currentUser?.getIdToken()),
-      },
-    }),
-  },
-});
-
-const authLink = setContext(async (_, { headers }) => {
-  const user = firebase.auth().currentUser;
-  console.log("user", user);
-  if (user) {
-    const token = await user.getIdToken();
-    return {
-      headers: {
-        ...headers,
-        authorization: token ? `Bearer ${token}` : "",
-      },
-    };
-  }
-  return {};
-});
-
-const splitLink = split(
-  ({ query }) => {
-    const definition = getMainDefinition(query);
-    return (
-      definition.kind === "OperationDefinition" &&
-      definition.operation === "subscription"
-    );
-  },
-  wsLink,
-  authLink.concat(httpLink)
-);
-
-const client = new ApolloClient({
-  cache: new InMemoryCache(),
-  link: splitLink,
-});
 
 const theme = deepMerge(generate(20), grommet, {
   global: { colors: { brand: "#0B74C2" } },
@@ -79,13 +21,13 @@ const theme = deepMerge(generate(20), grommet, {
 
 const App: React.FC = () => (
   <Grommet full theme={theme}>
-    <ApolloProvider client={client}>
+    <UrqlProvider value={client}>
       <BrowserRouter>
         <CommandsProvider>
           <Routes />
         </CommandsProvider>
       </BrowserRouter>
-    </ApolloProvider>
+    </UrqlProvider>
   </Grommet>
 );
 
